990f5a3a502048b6d5b3ad9280c8e48efb0c920c
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2024-04-07 12:38:41 -0300

    delete test

--- back-end-ecommerce/Ecommerce.Test/Seeding.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Seeding
-{
-    public static void InitializeTestDB(AppDbContext db)
-    {
-        db.Users.Add(new User { Email = "teste@teste.com", Password = "teste" });
-        db.SaveChanges();
-    }
-
-}
